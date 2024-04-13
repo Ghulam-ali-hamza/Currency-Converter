76846def066fcc04cd061c3c4c9ae600ecf6a2a5
Ghulam-ali-hamza <smartalikhandwala@gmail.com>
2024-04-13 18:26:22 +0500

    Create a currency converter that converts between major currencies with real-time exchange rates.

---
+++ main.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+const currency :any ={
+    USE :1,
+    EUR :0.91,
+    GBP :0.76,
+    INR :74.57,
+    PKR :200
+}
+
+let user_answer = await inquirer.prompt([
+    {
+     name:"from",
+     message: "Enter From Currency",
+     type: "list",
+     choices: ["USE","EUR","GBP","INR","PKR"]
+    },
+    {
+     name:"to",
+     message:"Enter From Currency",
+     type: "list",
+     choices: ["USE","EUR","GBP","INR","PKR"]   
+    },
+    {
+     name:"amount",
+     message:"Enter From Currency",
+     type: "number"
+    }
+])
+
+let fromamount = currency[user_answer.from]
+let toamount = currency[user_answer.to]
+let amount = user_answer.amount
+let baseamount =amount/fromamount
+let convertedamount= baseamount * toamount
+
+console.log(convertedamount);
